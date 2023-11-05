36c67f6f11cd5dc9d59108eba0cbcdc803bbe784
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-11-05 14:52:48 +0100

    Add a random generator function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern  crate rand;
+
+use rand::Rng;
+use rand::SeedableRng;
+use rand::rngs::StdRng;
+
+fn get_random_number(seed: &u64, index: &u64) -> u64
+{
+    let mut rng = StdRng::seed_from_u64(*seed);
+    for _ in 0..*index {
+        rng.gen::<u32>();
+    }
+    rng.gen_range(0..=100)
+}
+
 fn main() {
     println!("Hello, world!");
+    for i in 0..=10 {
+        let lol = get_random_number(&123456789, &i); // change this ...
+        println!("{}", lol);
+    }
+    
 }
